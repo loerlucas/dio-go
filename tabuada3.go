9ef221ee4deb5a56576e7c9833a5840a8f9b9ee3
loerlucas <loerlucas@gmail.com>
2025-06-11 15:47:39 -0300

    desafio 2 PIN PAN

--- tabuada3.go
+++ tabuada3.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-
+	//tabuada do 3
 	for i := 1; i < 101; i++ {
 		if i%3 == 0 {
 			fmt.Println(i)
@@ -11,5 +11,17 @@ func main() {
 			continue
 		}
 	}
+	//Bricadeira do Pin/Pan
+	for i := 1; i < 101; i++ {
 
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("PIN/PAN")
+		} else if i%3 == 0 {
+			fmt.Println("PIN")
+		} else if i%5 == 0 {
+			fmt.Println("PAN")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
